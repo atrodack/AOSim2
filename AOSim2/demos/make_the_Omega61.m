d24a1014561cbce2564c6f491b8680ab1287b6b3
Johanan L. Codona <jlcodona@gmail.com>
2011-03-07 23:45:58 -0700

    Reconstructor tests and masking off unilluminated actuators.

--- AOSim2/demos/make_the_Omega61.m
+++ AOSim2/demos/make_the_Omega61.m
@@ -80,7 +80,8 @@ DM.addActs(ACTS,1,A);
 
 % Specify the boundary conditions... 
 % DM.defineBC(5,8); % A circle of 8 null points at 5m radius.
-DM.defineBC(D,8); % A circle of 8 null points at D radius.
+% DM.defineBC(D/sqrt(2),16); % A circle of 8 null points at D radius.
+DM.defineBC(1,16); % A circle of 8 null points at D radius.
 DM.plotRegions; daspect([1 1 1]); drawnow;
 
 %% Build the Shack-Hartmann WFS.
@@ -91,19 +92,20 @@ A.show; WFS.quiver(1); drawnow; % Show them.
 
 %% Now for some real work.  Building the RECONSTRUCTOR...
 RECON = AOReconstructor(A,DM,WFS);
+RECON.lambda = AOField.VBAND;
 
 % Now program this crazy thing.
 % We can look at the singular values and choose things manually later.  
 % For now, we will make default assumptions.  You can rebuild it quickly
 % later.  The MMT currently runs with 56 modes corrected.
 
-RECON.adhocProgram(D/12*3);
-RECON.show;
+% RECON.adhocProgram(D/12*3);
+% RECON.show;
 
 % OWD = sqrt(MAX_MODES/pi);
 % % RECON.program(D,6*sqrt(2)); % Use Fourier modes. OWD is ~6 lambda/D for programming.
-% RECON.zprogram(D,12);  % program using Zernikes.
-% RECON.rebuild(56).show;
+RECON.zprogram(D,12);  % program using Zernikes.
+RECON.rebuild(56).show;
 
 % % RECON.dhprogram(D,11); % program using disk harmonics.
 % semilogy(RECON.s/RECON.s(1));
