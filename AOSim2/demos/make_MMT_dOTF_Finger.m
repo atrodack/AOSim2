59b8c41feecbf1c76697a05284f81dc1afddc61a
Johanan L. Codona <jlcodona@gmail.com>
2012-08-17 10:07:02 -0700

    Added some old demo files for the LX200 and the MMT dOTF finger.

---
+++ AOSim2/demos/make_MMT_dOTF_Finger.m
@@ -0,0 +1,62 @@
+% I am assuming that the AOSim2 data directory is here.
+% A symlink is good enough.
+
+LAMBDA = AOField.JBAND;
+
+make_the_MMT_AO_jlc
+
+thld = LAMBDA/D*206265;
+FOV = 25 * thld;
+PLATE_SCALE = thld/3;
+
+
+FINGER = AOSegment(A)
+FINGER.name = 'The Finger!';
+
+[X,Y] = FINGER.COORDS;
+
+% Just put a mask into the grid directly...
+FINGER.grid(~(Y<-2.5 & abs(X-1)<0.25));
+
+F = AOField(A);
+F.lambda = LAMBDA;
+F.FFTSize = 1024;
+
+N1 = 3; 
+N2 = 2;
+
+subplot(N1,N2,1);
+F.planewave*A;
+F.show
+PSF0 = F.mkPSF(FOV,PLATE_SCALE);
+OTF0 = ifftshift(fft2(fftshift(PSF0)));
+
+subplot(N1,N2,2);
+F*FINGER;
+F.show
+PSF1 = F.mkPSF(FOV,PLATE_SCALE);
+OTF1 = ifftshift(fft2(fftshift(PSF1)));
+
+subplot(N1,N2,N2+1);
+imagesc([PSF0 PSF1].^.25);
+daspect([1 1 1]);
+axis off;
+
+subplot(N1,N2,N2+2);
+imagesc(abs([OTF0 OTF1]));
+daspect([1 1 1]);
+axis xy;
+axis off;
+
+subplot(N1,N2,2*N2+1);
+imagesc(abs(OTF0-OTF1));
+daspect([1 1 1]);
+axis xy;
+axis off;
+
+subplot(N1,N2,2*N2+2);
+imagesc(angle(OTF0-OTF1));
+daspect([1 1 1]);
+axis xy;
+axis off;
+
